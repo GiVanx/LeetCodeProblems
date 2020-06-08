9bd8a52adb3deb8378eadea60b5aaeae9685e006
Ivan Gandacov <gandacov96@gmail.com>
2020-06-08 08:01:36 +0200

    [88] Merge sorted arrays

---
+++ 88_MergeSortedArray/src/com/leetcode/Solution.java
@@ -0,0 +1,46 @@
+package com.leetcode;
+
+/**
+ * 88. Merge sorted array
+ *
+ * https://leetcode.com/problems/merge-sorted-array/
+ *
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ *
+ *     The number of elements initialized in nums1 and nums2 are m and n respectively.
+ *     You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ *
+ * Example:
+ *
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * Output: [1,2,2,3,5,6]
+ *
+ */
+public class Solution {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int lo = m - 1;
+        int hi = nums1.length - 1;
+        int k = nums2.length - 1;
+
+        while (k >= 0) {
+            while (lo >= 0 && nums1[lo] > nums2[k]) {
+                nums1[hi] = nums1[lo];
+                --hi;
+                --lo;
+            }
+            nums1[hi] = nums2[k];
+            --hi;
+            --k;
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
